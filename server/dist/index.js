d40c727f55cd7e2719ef9433d06a67af48e09a99
HelixHEX <heyy.im.eli@gmail.com>
2020-09-29 16:22:52 -0400

    removed async

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -37,10 +28,10 @@ const main = () => {
         console.log(messages);
         res.send(messages);
     });
-    app.get("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    app.get("/login", (req, res) => {
         const username = req.query.username;
         const password = req.query.password;
-        yield users.forEach((user) => {
+        users.forEach(user => {
             if (username == user.username) {
                 if (password == user.password) {
                     res.send({ response: "success" });
@@ -53,12 +44,12 @@ const main = () => {
                 res.send({ response: "Incorrect username/password" });
             }
         });
-    }));
-    app.get("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    });
+    app.get("/signup", (req, res) => {
         const username = req.query.username;
         const password = req.query.password;
         var exists = false;
-        yield users.forEach((user) => {
+        users.forEach((user) => {
             if (username == user.username) {
                 exists = true;
             }
@@ -73,7 +64,7 @@ const main = () => {
             });
             res.send({ "response": "success" });
         }
-    }));
+    });
     app.listen(process.env.PORT || 5000, () => {
         console.log("Server started");
     });
