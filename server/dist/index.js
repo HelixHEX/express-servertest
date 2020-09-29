e515211d780dbcff7229acc1c8b99429883ec743
HelixHEX <heyy.im.eli@gmail.com>
2020-09-29 10:45:58 -0400

    First commit

---
+++ server/dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const main = () => {
+    const app = express_1.default();
+    const users = [
+        {
+            name: "Test User",
+            username: "randomusername"
+        },
+        {
+            name: "Test User",
+            username: "randomusername"
+        },
+        {
+            name: "Test User",
+            username: "randomusername"
+        },
+    ];
+    app.get("/allusers", (_, res) => {
+        res.send(users);
+    });
+    app.listen(5000, () => {
+        console.log("Server started");
+    });
+};
+main();
+//# sourceMappingURL=index.js.map
---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express, {Response} from 'express'
+
+const main = () => {
+  const app = express()
+
+  const users = [
+    {
+      name: "Test User",
+      username: "randomusername"
+    },
+    {
+      name: "Test User",
+      username: "randomusername"
+    },
+    {
+      name: "Test User",
+      username: "randomusername"
+    },
+  ]
+
+  app.get("/allusers", (_,res: Response) => {
+    res.send(users)
+  })
+
+  app.listen(5000, () => {
+    console.log("Server started")
+  })
+}
+
+main()
