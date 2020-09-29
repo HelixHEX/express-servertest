7f7e1e17bb9535d37581c16017b52f2f01f03c04
HelixHEX <heyy.im.eli@gmail.com>
2020-09-29 12:04:43 -0400

    Added login

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,25 +6,41 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const main = () => {
     const app = express_1.default();
-    const users = [
-        {
-            name: "Test User",
-            username: "randomusername"
-        },
-        {
-            name: "Test User",
-            username: "randomusername"
-        },
-        {
-            name: "Test User",
-            username: "randomusername"
-        },
-    ];
+    var users = [{
+            username: "test",
+            password: "test"
+        }];
+    var messages = [];
     app.get("/", (_, res) => {
         res.send("Hello world");
     });
     app.get("/allusers", (_, res) => {
         res.send(users);
+    });
+    app.get("/loadmessages", (_, res) => {
+        res.send(messages);
+    });
+    app.get("/sendmessage", (req, res) => {
+        const message = req.body.message;
+        messages.push(message);
+        res.send(messages);
+    });
+    app.get("/login", (req, res) => {
+        const username = req.query.username;
+        const password = req.query.password;
+        users.forEach(user => {
+            if (username == user.username) {
+                if (password == user.password) {
+                    res.send("success");
+                }
+                else {
+                    res.send("Incorrect username/password");
+                }
+            }
+            else {
+                res.send("Incorrect username/password");
+            }
+        });
     });
     app.listen(process.env.PORT || 5000, () => {
         console.log("Server started");
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,27 +2,43 @@ import express, {Response} from 'express'
 
 const main = () => {
   const app = express()
-
-  const users = [
-    {
-      name: "Test User",
-      username: "randomusername"
-    },
-    {
-      name: "Test User",
-      username: "randomusername"
-    },
-    {
-      name: "Test User",
-      username: "randomusername"
-    },
-  ]
+  var users = [{
+    username: "test",
+    password: "test"
+  }]
+  var messages:Object[] = []
 
   app.get("/", (_, res: Response) => {
     res.send("Hello world")
   })
   app.get("/allusers", (_,res: Response) => {
     res.send(users)
+  })
+
+  app.get("/loadmessages", (_, res: Response) => {
+    res.send(messages)
+  })
+
+  app.get("/sendmessage", (req, res:Response) => {
+    const message = req.body.message;
+    messages.push(message)
+    res.send(messages)
+  })
+
+  app.get("/login", (req, res:Response) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    users.forEach(user => {
+      if (username == user.username) {
+        if (password == user.password) {
+          res.send("success")
+        } else {
+          res.send("Incorrect username/password");
+        }
+      } else {
+        res.send("Incorrect username/password");
+      }
+    })
   })
 
   app.listen(process.env.PORT || 5000, () => {
